f5a8bff8fe41aea1ec9e4827072c9bc44eb1ba79
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2024-06-06 10:57:30 +0200

    feat: added entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z "$PACKAGE_MANAGER" ]; then
+    echo "PACKAGE_MANAGER not defined."
+    exit 1
+fi
+
+if [ $RUN_PACKAGE_MANAGER -eq 0 ]; then
+    sleep infinity
+    exit 0
+fi
+
+case "$PACKAGE_MANAGER" in
+yarn)
+    yarn install $PACKAGE_MANAGER_INSTALL_ARGS
+    yarn $@
+    ;;
+npm)
+    npm i $PACKAGE_MANAGER_INSTALL_ARGS
+    npm $@
+    ;;
+pnpm)
+    pnpm i $PACKAGE_MANAGER_INSTALL_ARGS
+    pnpm $@
+    ;;
+*)
+    echo "PACKAGE_MANAGER has unknown value. Use 'yarn', 'npm' or 'pnpm'."
+    exit 1
+    ;;
+esac
